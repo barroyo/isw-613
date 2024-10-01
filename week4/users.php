7f659a373b9bba8599e4515737e37b13f92e09cb
Bladimir Arroyo <bladimir.ab@gmail.com>
2024-10-01 10:43:42 -0600

    Week 4 example missing

---
+++ week4/users.php
@@ -0,0 +1,33 @@
+<?php
+  include('utils/functions.php');
+?>
+<?php require('inc/header.php')?>
+  <div class="container-fluid">
+    <div class="jumbotron">
+      <h1 class="display-4">Users</h1>
+      <p class="lead">List of users</p>
+      <hr class="my-4">
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Lane</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td scope="row"></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td scope="row"></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+<?php require('inc/footer.php');
