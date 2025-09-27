020616427ad7408f04730b6729860afe6cd51efd
Bladimir Arroyo <bladimir.ab@gmail.com>
2025-09-26 18:53:15 -0600

    week 2

---
+++ week2/print.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Print</title>
+</head>
+<body>
+<?php
+
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+echo $username;
+echo "<br>";
+echo $password
+
+?>
+
+</body>
+</html>
